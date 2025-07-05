4de4e81789f449e62b7a5bcdaab05a07ba09f2cf
srishtikapoor22 <srishtikapoor2201@gmail.com>
2025-07-05 16:19:58 +0530

    Added TechStack component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Hero from './components/Hero';
+import TechStack from './components/TechStack';
 
 function App() {
   return (
     <div>
       <Hero />
+      <TechStack />
     </div>
   );
 }
